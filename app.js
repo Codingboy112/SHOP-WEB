0436e108532ebbdc0c83445fb2099c4575ba6146
tyler <tylertheunknownss@gmail.com>
2025-05-20 11:11:28 +0500

    half site

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 let headerCloseBtn = document.querySelector(".closeTheHead");
 let headerWillTop = document.querySelector(".header__wrapper-top__color");
+
+// headerDropdown
+
 let dropdownHeader = document.querySelector(".dropdown__header");
 let openDropdown = document.querySelector(".openDropdown");
+
+//comment carousel
+
+const cardList = document.querySelector(".card-list");
+const leftArrow = document.querySelector(".bi-arrow-left");
+const rightArrow = document.querySelector(".bi-arrow-right");
 
 headerCloseBtn.addEventListener("click", () => {
   headerWillTop.remove();
@@ -9,4 +18,21 @@ headerCloseBtn.addEventListener("click", () => {
 
 openDropdown.addEventListener("click", () => {
   dropdownHeader.classList.toggle("showDropdown");
+});
+
+
+
+const scrollAmount = 370;
+rightArrow.addEventListener("click", () => {
+  cardList.scrollBy({
+    left: scrollAmount,
+    behavior: "smooth",
+  });
+});
+
+leftArrow.addEventListener("click", () => {
+  cardList.scrollBy({
+    left: -scrollAmount,
+    behavior: "smooth",
+  });
 });
